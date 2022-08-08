bf313a423d923daa3282f2fd03ec7d7b17f9b1d2
semava0226 <smvalencia024@misena.edu.co>
2022-08-08 11:54:30 -0500

    videos
    videos

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,8 +11,7 @@
     p Le damos la bienvenida al componente formativo “Bases técnicas para la formulación de proyectos”; comience esta experiencia visualizando con atención el recurso que se muestra enseguida. ¡Adelante!
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-    
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DpSOzr4zYu4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -946,8 +946,7 @@
     p Explore el siguiente recurso y profundice en las generalidades clave de la implementación de un proyecto:  
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-                                                                                                                                                                                     
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/fhKAHF7FSRU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
